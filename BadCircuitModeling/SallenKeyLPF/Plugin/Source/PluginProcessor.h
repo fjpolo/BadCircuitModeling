e61a8a953a2b852fc2b930940f85f2057b3f5341
franco.polo <franco.polo@ii-vi.com>
2021-07-29 17:36:18 +0200

    - BadCircuitModeling\SallenKeyLPF
    - BadCircuitModeling\OpAmp
    - BadCircuitModeling\Paper

---
+++ BadCircuitModeling/SallenKeyLPF/Plugin/Source/PluginProcessor.h
@@ -0,0 +1,89 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "OpAmpLPF.h"
+#include "LPFCircuit.h"
+
+//==============================================================================
+/**
+*/
+class SallenKeyLpfAudioProcessor  : public AudioProcessor
+{
+public:
+    //==============================================================================
+    SallenKeyLpfAudioProcessor();
+    ~SallenKeyLpfAudioProcessor();
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (AudioBuffer<float>&, MidiBuffer&) override;
+
+    //==============================================================================
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const String getProgramName (int index) override;
+    void changeProgramName (int index, const String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    OwnedArray<AudioProcessorValueTreeState>& getVTS() { return vts; }
+    static AudioProcessorValueTreeState::ParameterLayout createParameterLayout (int type);
+
+    LPF& getLPF (int ch) { return lpf[ch]; }
+    void updateParams();
+
+private:
+    // circuit
+    float* freqParam;
+    float* qParam;
+
+    // components
+    float* cTolParam;
+    float* cAgeParam;
+    float* cTempParam;
+    float* cCapFailParam;
+
+    // op amp
+    float* oaTempParam;
+    float* oaAgeParam;
+
+    OpAmpLPF lpf[2];
+    LPFCircuit circuit[2];
+    float rmsLevel[2] = { 0.0f, 0.0f };
+
+    OwnedArray<AudioProcessorValueTreeState> vts;
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SallenKeyLpfAudioProcessor)
+};
